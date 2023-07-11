3aa3b40265aaec6ba14ba316c290d6355c995b7c
takmuzunze <takmuzunze@gmail.com>
2023-07-11 11:57:52 +0100

    update scripts.js - added messsgeinput variable for form validation

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
 const submitButton = document.getElementById('submit');
 const emailInput = document.getElementById('email');
 const nameInput = document.getElementById('fullname');
+const messageInput = document.getElementById('message');
