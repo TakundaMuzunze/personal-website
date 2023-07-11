1580ad72e2a05ecb4963732d2bd45c80b93260c9
takmuzunze <takmuzunze@gmail.com>
2023-07-11 12:25:56 +0100

    update js - added if statement for form validation to check if user input is empty

--- scripts.js
+++ scripts.js
@@ -2,3 +2,11 @@ const submitButton = document.getElementById('submit');
 const emailInput = document.getElementById('email');
 const nameInput = document.getElementById('fullname');
 const messageInput = document.getElementById('message');
+const submissionMessage = document.getElementsByClassName('.form-message');
+
+if (emailInput.value==="" && nameInput.value=== ""){
+    alert('Please fill out details correctly');
+}
+else{
+    submissionMessage.classList.add('form-message-shown');
+}
