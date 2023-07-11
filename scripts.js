85b0c0d84956eafc18963f2205762eb0b2ec906f
takmuzunze <takmuzunze@gmail.com>
2023-07-11 13:27:13 +0100

    update js - fixed event listener on submit button, assigned submission message variable to the correct id name, correctly displayed successful submission message

--- scripts.js
+++ scripts.js
@@ -2,17 +2,18 @@ const submitButton = document.getElementById('submit');
 const emailInput = document.getElementById('email');
 const nameInput = document.getElementById('fullname');
 const messageInput = document.getElementById('message');
-const submissionMessage = document.getElementsByClassName('.form-message');
+const submissionMessage = document.getElementById('displayed-message');
 
 const formValidation = () => {
-    if (emailInput.value === "" && nameInput.value === "") {
+    if (emailInput.value === "" && nameInput.value === "" && messageInput.value === "") {
         alert('Please fill out your details in the form correctly');
     }
     else {
         submissionMessage.classList.add('form-message-shown');
+        submitButton.style.display= "none";
     }
 };
 
-submitButton.addEventListener(click, function() {
+submitButton.addEventListener('click', function() {
     formValidation();
 });
